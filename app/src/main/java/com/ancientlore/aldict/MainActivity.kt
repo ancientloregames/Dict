85fc0de505085938c8f6a468dae6d40fec9faa46
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-02-24 21:42:16 +0200

    init

---
+++ app/src/main/java/com/ancientlore/aldict/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ancientlore.aldict
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(previousState: Bundle?) {
+    super.onCreate(previousState)
+    setContentView(R.layout.activity_main)
+  }
+}
