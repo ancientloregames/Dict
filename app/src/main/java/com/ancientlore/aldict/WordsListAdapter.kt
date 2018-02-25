9f435b6ab901f24669aca9aab6fc77ea374e872f
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-02-25 17:13:02 +0200

    added list view

---
+++ app/src/main/java/com/ancientlore/aldict/WordsListAdapter.kt
@@ -0,0 +1,25 @@
+package com.ancientlore.aldict
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * com.ancientlore.aldict. Created by nimblemind on 2/25/2018.
+ */
+class WordsListAdapter(val items : List<Word>) : RecyclerView.Adapter<WordsListAdapter.ViewHolder>() {
+
+  override fun getItemCount(): Int {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView)
+}
