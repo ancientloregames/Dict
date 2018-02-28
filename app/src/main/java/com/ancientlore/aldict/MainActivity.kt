0e34d300d1f57c8ef4bc81628c61d138c5ef2a8c
nimblemind <ancientloregames@gmail.com>
2018-02-28 09:27:11 +0200

    turned on DataBinding

--- app/src/main/java/com/ancientlore/aldict/MainActivity.kt
+++ app/src/main/java/com/ancientlore/aldict/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.ancientlore.aldict
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import java.util.concurrent.ExecutorService
