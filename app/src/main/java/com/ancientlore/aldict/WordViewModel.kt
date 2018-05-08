1e7efd84429999c9b21d4f87b1ff4b17cac313dc
Nickolas Makhyboroda <ancientloregames@gmail.com>
2018-05-08 09:32:48 +0200

    implemented basic functionality of synonyms field

--- app/src/main/java/com/ancientlore/aldict/WordViewModel.kt
+++ app/src/main/java/com/ancientlore/aldict/WordViewModel.kt
@@ -1,6 +1,8 @@
 package com.ancientlore.aldict
 
+import android.databinding.BindingAdapter
 import android.databinding.ObservableField
+import android.view.View
 
 class WordViewModel : BaseViewModel() {
 
@@ -10,10 +12,38 @@ class WordViewModel : BaseViewModel() {
 
 	val transcription = ObservableField<String>("")
 
+	val synonym = ObservableField<String>("")
+
+	val synonyms = ObservableField<String>("")
+
+	val note = ObservableField<String>("")
+
 	internal fun getWord(): Word {
 		val name = this.name.get()!!
 		val translations = this.translations.get()!!
 		val transcription = this.transcription.get()!!
-		return Word(name = name, translations = translations, transcription = transcription)
+		val synonyms = this.synonyms.get()!!
+		val note = this.note.get()!!
+		return Word(name = name, translations = translations, transcription = transcription, note = note, synonyms = synonyms)
+	}
+
+	fun synonymsListVisibility() = if (synonyms.get()!!.isNotEmpty()) View.VISIBLE else View.GONE
+
+	fun onAddSynonymClicked() {
+		synonym.get()?.let {
+			if(isSynonymNew(it)) addSynonym(it)
+		}
+	}
+
+	private fun isSynonymNew(candidate: String) = synonyms.get()?.split(", ")?.all { it != candidate } ?: false
+
+	private fun addSynonym(synonym: String) {
+		val newSynonyms = StringBuilder(synonyms.get()!!)
+
+		if (newSynonyms.isNotEmpty()) newSynonyms.append(", ")
+
+		newSynonyms.append(synonym)
+
+		synonyms.set(newSynonyms.toString())
 	}
 }
